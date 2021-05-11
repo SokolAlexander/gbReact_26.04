1e04e38c5be79a50177ee2b16a47a30655edb747
SokolAlexander <asokol1994@gmail.com>
2021-05-11 15:39:32 +0300

    console to test

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import ReactDOM from "react-dom";
 import { Routes } from './Routes';
 import { Parent } from "./components/Parent";
 
+console.warn('working with router');
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
